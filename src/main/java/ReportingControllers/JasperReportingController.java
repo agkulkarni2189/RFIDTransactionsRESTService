b4222146f449fa447636c7a0bb76eec127513c74
Aniket Kulkarni <aniket.kulkarni@surajinformatics.com>
2019-01-04 17:00:43 +0530

    initial commit

--- src/main/java/ReportingControllers/JasperReportingController.java
+++ src/main/java/ReportingControllers/JasperReportingController.java
@@ -1,8 +1,28 @@
 package ReportingControllers;
 
+import java.io.StringWriter;
+import java.sql.SQLException;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import haziraDBAPIs.TransactionAPI;
+import ar.com.fdvs.dj.core.DynamicJasperHelper;
+import ar.com.fdvs.dj.core.layout.ClassicLayoutManager;
+import ar.com.fdvs.dj.domain.DynamicReport;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRCsvExporter;
+import net.sf.jasperreports.engine.export.JRTextExporter;
+import net.sf.jasperreports.engine.export.JRXmlExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleTextExporterConfiguration;
+import net.sf.jasperreports.export.SimpleTextReportConfiguration;
+import net.sf.jasperreports.export.SimpleWriterExporterOutput;
+import net.sf.jasperreports.export.SimpleXmlExporterOutput;
+import ReportingUtility.DynamicReportingUtil;
 
 @RestController
 public class JasperReportingController {
@@ -12,9 +32,103 @@ public class JasperReportingController {
 	}
 	
 	@RequestMapping("/reportingService")
-	public String getJasperReport(@RequestParam(value="LocationID", defaultValue="") String LocationID, @RequestParam(value="LaneTypeID", defaultValue="") String LaneTypeID, @RequestParam(value="LaneID", defaultValue="") String LaneID)
+	public String getJasperReport(@RequestParam(value="LocationID", defaultValue="") String LocationID, @RequestParam(value="LaneTypeID", defaultValue="") String LaneTypeID, @RequestParam(value="LaneID", defaultValue="") String LaneID, @RequestParam(value="ExportOption", defaultValue="1") String ExportOption)
 	{
-		return null;
+		String ExportedReport = new String();
+		JasperPrint jp;
+		try 
+		{
+			jp = this.getJasperPrint(LocationID, LaneTypeID, LaneID);
+			int eo = Integer.parseInt(ExportOption);
+			
+			switch (eo)
+			{
+			case 1: ExportedReport = this.ExportReportToXML(jp);
+			break;
+			case 2: ExportedReport = this.ExportReportToCSV(jp);
+			break;
+			case 3: ExportedReport = this.StringExportReportToText(jp);
+			break;
+			default: ExportedReport = "Invalid export option";
+			break;
+			}
+		} catch (JRException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		return ExportedReport;
+	}
+	
+	private String ExportReportToXML(JasperPrint jp)
+	{
+		StringWriter XmlReportWriter = new StringWriter();
 		
+		try
+		{
+			SimpleXmlExporterOutput xmlOutput = new SimpleXmlExporterOutput(XmlReportWriter);
+			xmlOutput.setEmbeddingImages(true);
+			
+			JRXmlExporter xmlExporter = new JRXmlExporter();
+			xmlExporter.setExporterInput(new SimpleExporterInput(jp));
+			xmlExporter.setExporterOutput(xmlOutput);
+			xmlExporter.exportReport();
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		
+		return XmlReportWriter.toString();
+	}
+	
+	private String ExportReportToCSV(JasperPrint jp) throws JRException
+	{
+		StringWriter sw = new StringWriter();
+		JRCsvExporter CSVExporter = new JRCsvExporter();
+		SimpleExporterInput ExporterInput = new SimpleExporterInput(jp);
+		SimpleWriterExporterOutput WriterOutput = new SimpleWriterExporterOutput(sw);
+		CSVExporter.setExporterInput(ExporterInput);
+		CSVExporter.setExporterOutput(WriterOutput);
+		CSVExporter.exportReport();
+		return sw.toString();
+	}
+	
+	private String StringExportReportToText(JasperPrint jp) throws JRException
+	{
+		JRTextExporter TextExporter = new JRTextExporter();
+		StringWriter sw = new StringWriter();
+		SimpleExporterInput ExporterInput = new SimpleExporterInput(jp);
+		SimpleWriterExporterOutput WriterOutput = new SimpleWriterExporterOutput(sw);
+		
+		SimpleTextExporterConfiguration TextExporterConfiguration = new SimpleTextExporterConfiguration ();
+		TextExporterConfiguration.setLineSeparator(",");
+		TextExporterConfiguration.setPageSeparator(".");
+		TextExporterConfiguration.setOverrideHints(false);
+		TextExporter.setConfiguration(TextExporterConfiguration);
+		
+		SimpleTextReportConfiguration TextReportConfiguration = new SimpleTextReportConfiguration();
+		TextReportConfiguration.setOverrideHints(false);
+		TextReportConfiguration.setCharWidth(9.0f);
+		TextReportConfiguration.setCharHeight(9.0f);
+		TextExporter.setConfiguration(TextReportConfiguration);
+		TextExporter.setExporterInput(ExporterInput);
+		TextExporter.setExporterOutput(WriterOutput);
+		
+		TextExporter.exportReport();
+		return sw.toString();
+	}
+	
+	public JasperPrint getJasperPrint(String LocationID, String LaneTypeID, String LaneID) throws JRException, SQLException
+	{
+		DynamicReportingUtil dru = new DynamicReportingUtil();
+		DynamicReport report = dru.buildJasperReport(LocationID, LaneTypeID, LaneID);
+		TransactionAPI RFIDTransAPI = new TransactionAPI();
+		JRBeanCollectionDataSource TransactionDS = new JRBeanCollectionDataSource(RFIDTransAPI.getRFIDTransactions(LocationID, LaneTypeID, LaneID));
+		return DynamicJasperHelper.generateJasperPrint(report, new ClassicLayoutManager(), TransactionDS);
+		//return null;
 	}
 }
--- src/main/java/ReportingUtility/DynamicReportingUtil.java
+++ src/main/java/ReportingUtility/DynamicReportingUtil.java
@@ -1,9 +1,98 @@
 package ReportingUtility;
 
+import java.awt.Color;
+import java.sql.Date;
+
+import ar.com.fdvs.dj.core.DJConstants;
+import ar.com.fdvs.dj.domain.DynamicReport;
+import ar.com.fdvs.dj.domain.Style;
+import ar.com.fdvs.dj.domain.builders.ColumnBuilder;
+import ar.com.fdvs.dj.domain.builders.DynamicReportBuilder;
+import ar.com.fdvs.dj.domain.builders.FastReportBuilder;
+import ar.com.fdvs.dj.domain.constants.Font;
+import ar.com.fdvs.dj.domain.constants.HorizontalAlign;
+import ar.com.fdvs.dj.domain.constants.Transparency;
+import ar.com.fdvs.dj.domain.constants.VerticalAlign;
+import ar.com.fdvs.dj.domain.entities.columns.AbstractColumn;
+
 public class DynamicReportingUtil {
 
 	public DynamicReportingUtil() {
 		// TODO Auto-generated constructor stub
+		
 	}
+	
+	public DynamicReport buildJasperReport(String LocationID, String LaneTypeID, String LaneID)
+	{
+		StringBuilder DBQuery = new StringBuilder();
+		StringBuilder WhereCondition = new StringBuilder();
+		
+		DynamicReportBuilder drb = new DynamicReportBuilder();
+		
+		Style HeaderStyle = new Style();
+		HeaderStyle.setHorizontalAlign(HorizontalAlign.CENTER);
+		HeaderStyle.setVerticalAlign(VerticalAlign.MIDDLE);
+		HeaderStyle.setBackgroundColor(Color.GRAY);
+		HeaderStyle.setTransparency(Transparency.OPAQUE);
+		HeaderStyle.setTransparent(false);
+		Font HeaderFont = new Font();
+		HeaderFont.setBold(true);
+		HeaderFont.setFontName(Font.ARIAL_BIG.getFontName());
+		HeaderFont.setFontSize(16.0f);
+		HeaderStyle.setFont(HeaderFont);
 
+		Style ContentStyle = new Style();
+		ContentStyle.setHorizontalAlign(HorizontalAlign.CENTER);
+		ContentStyle.setVerticalAlign(VerticalAlign.MIDDLE);
+		ContentStyle.setBackgroundColor(Color.LIGHT_GRAY);
+		ContentStyle.setTransparency(Transparency.OPAQUE);
+		ContentStyle.setTransparent(false);
+		Font ContentFont = new Font();
+		ContentFont.setFontName(Font.ARIAL_SMALL.getFontName());
+		ContentFont.setFontSize(13.0f);
+		ContentStyle.setFont(ContentFont);
+		
+		AbstractColumn TagIDCol = ColumnBuilder.getNew().setColumnProperty("tag_id", Integer.class.getName())
+				.setTitle("RFID Tag ID").setWidth(100).build();
+
+		AbstractColumn ReqSrcID = ColumnBuilder.getNew()
+				.setColumnProperty("boom_up_request_source", Integer.class.getName()).setTitle("Boom Up Request Source")
+				.setWidth(100).build();
+
+		AbstractColumn ReqSentTime = ColumnBuilder.getNew()
+				.setColumnProperty("ack_sent_time", Date.class.getName())
+				.setTitle("Tag Request Sent To Server Time").setWidth(150).build();
+
+		AbstractColumn HHTBoomUpReqReceivedTime = ColumnBuilder.getNew()
+				.setColumnProperty("hht_boomup_request_recd_time", Date.class.getName())
+				.setTitle("HHT Boom Up Req Receive Time").setWidth(150).build();
+
+		AbstractColumn RFIDBoomUpReqReceivedTime = ColumnBuilder.getNew()
+				.setColumnProperty("rfid_boomup_request_recd_time", Date.class.getName())
+				.setTitle("RFID Boom Up Req Receive Time").setWidth(150).build();
+
+		AbstractColumn Lane = ColumnBuilder.getNew().setColumnProperty("lane_name", String.class.getName())
+				.setTitle("Lane").setWidth(150).build();
+
+		AbstractColumn Location = ColumnBuilder.getNew().setColumnProperty("location_name", String.class.getName())
+				.setTitle("Location").setWidth(150).build();
+
+		AbstractColumn Lane_Type_Name = ColumnBuilder.getNew()
+				.setColumnProperty("lane_type_name", String.class.getName()).setTitle("Lane Type").setWidth(150)
+				.build();
+		
+		FastReportBuilder frb = (FastReportBuilder) new FastReportBuilder()
+				.addColumn(TagIDCol)
+				.addColumn(ReqSrcID)
+				.addColumn(ReqSentTime)
+				.addColumn(HHTBoomUpReqReceivedTime)
+				.addColumn(RFIDBoomUpReqReceivedTime)
+				.addColumn(Lane)
+				.addColumn(Lane_Type_Name)
+				.addColumn(Location)
+				.setDefaultStyles(null, null, HeaderStyle, ContentStyle)
+				.setUseFullPageWidth(true);
+		
+		return frb.build();
+	}
 }
